47e9604ea531a09c4168fa9deda25116ea5427e1
Jaikishan <jaikishangupta754@gmail.com>
2026-05-07 01:37:44 +0530

    creating login and registeration page - with the fields name, email, password

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
