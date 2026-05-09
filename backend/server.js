ac169ac15e7e4431a83dc7f6526fbcfaaf86245d
Jaikishan <jaikishangupta754@gmail.com>
2026-05-09 10:36:38 +0530

    adding the frontend url the the cors allow on backend

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: "hhttps://dacby-t.vercel.app/", // frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
