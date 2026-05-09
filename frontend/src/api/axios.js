7e39ad0447c08a59a3c1d27066a9230e76f902e5
Jaikishan <jaikishangupta754@gmail.com>
2026-05-09 10:29:25 +0530

    adding the live backen url to the frontend env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// console.log("url", `${import.meta.env.VITE_BACKEND_URL}`);
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 export default api;
